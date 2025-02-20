498c1359c77ef4750e61d9441309d7a53a15fb52
Jessica Lorenzo <jessicalorenzo32@gmail.com>
2025-02-20 14:13:42 -0500

    add css components

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ const colorForm = document.querySelector(".color");
 
 colorForm.addEventListener("submit", async function (event) {
   event.preventDefault();
-  const colorSelection = document.querySelector(".color__selection").value;
-  await getPokemonSpecies(colorSelection);
+  const colorSelection = document.querySelector(".color__selection");
+  await getPokemonSpecies(colorSelection.value);
 });
 
 async function getPokemonSpecies(userColorInput) {
   try {
     const response = await axios.get(`${url}pokemon-color/${userColorInput}`);
     const pokeSpecies = response.data.pokemon_species;
+    console.log(pokeSpecies);
     const randomNumber = Math.floor(Math.random() * pokeSpecies.length);
     const randomPokemon = pokeSpecies[randomNumber];
+    console.log(randomPokemon);
     const pokemonResponse = await axios.get(
       `${url}pokemon/${randomPokemon.name}`
     );
+    console.log(pokemonResponse);
     const pokemonImage = pokemonResponse.data.sprites.front_default;
     randomPokemon.image = pokemonImage;
 
@@ -29,14 +32,17 @@ async function getPokemonSpecies(userColorInput) {
 function populateSpeciesContainer(pokemonObject) {
   const speciesContainer = document.querySelector(".pokemon");
   const pokemonContainer = document.createElement("div");
-
-  const pokeNameElement = document.createElement("p");
-  pokeNameElement.textContent = pokemonObject.name;
-  pokemonContainer.appendChild(pokeNameElement);
+  pokemonContainer.classList.add("pokemon__container");
 
   const pokeImageElement = document.createElement("img");
   pokeImageElement.src = pokemonObject.image;
+  pokeImageElement.classList.add("pokemon__image");
   pokemonContainer.appendChild(pokeImageElement);
+
+  const pokeNameElement = document.createElement("p");
+  pokeNameElement.textContent = pokemonObject.name;
+  pokeNameElement.classList.add("pokemon__name");
+  pokemonContainer.appendChild(pokeNameElement);
 
   speciesContainer.replaceChildren(pokemonContainer);
 }
